1cbb451d1383a1e1da45df2efaeb2b8bba2217c9
Shreyansh Modi <shrynshmodi@gmail.com>
2025-06-24 01:01:25 +0530

    Create home.js

---
+++ assets/js/home.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  new Typed('#typed', {
+    strings: ['a developer!', 'an AI researcher!', 'passionate about CV!'],
+    typeSpeed: 80,
+    backSpeed: 40,
+    loop: true,
+    backDelay: 2000
+  });
+});
